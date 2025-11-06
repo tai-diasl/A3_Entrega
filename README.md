e9522f186dfd7500d54f478b6298ade3fb3a8f40
Taís Dias <taisayana@hotmail.com>
2025-11-06 18:55:45 +0000

    feat: entrega da aula 3 e ficheiro info.js

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # A3_Entrega
+Estou muito feliz em escrever o meu primeiro commit do módulo.
+Taís Ayana Dias Lindolfo 221
---
+++ info.js
@@ -0,0 +1,11 @@
+// comida
+console.log("Os meus pratos favoritos são pizza e feijoada com batata frita!")
+
+// bebida
+console.log("A minha bebida favorita é Água de Coco!")
+
+// cor
+console.log("A minha cor favorita é roxo!")
+
+// hobbie
+console.log("Eu gosto muito de viajar!")
